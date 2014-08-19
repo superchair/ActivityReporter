844d2b6bfc32101179fe12acc754ba26fdd4c2c8
Spencer Gilroy <spencer.gilroy+github@gmail.com>
2014-08-19 11:15:12 -0400

    Controller foundation for displaying form
    Added the foundations for launching a form when the edit or add buttons
    are pressed.

--- src/js/main.js
+++ src/js/main.js
@@ -14,11 +14,21 @@
         $scope.buildVersion = envBuildVersion;
 
         $scope.items = [{text:'Top Item', subItems:[{text:'I am a top level item'}, {text:'I am also a top level item'}]},{text:'Second Item', subItems:[]}];
+
+        $scope.createNewTopItem = function(){
+            console.log('Add new Topic Item!!');
+        }
     }]);
 
     activityReporter.controller('dialogCtrl', ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
-        $scope.launch = function() {
-            console.log("Hi you pressed me :P");
+        $scope.newSubItem = function(parentItem) {
+            console.log('Add new subitem');
+            console.log(parentItem);
+        };
+
+        $scope.editItem = function(item){
+            console.log('EDIT ME!!');
+            console.log(item);
         };
     }]);
 
