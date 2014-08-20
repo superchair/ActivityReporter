b2e8f50f03e3d02d95d191f99994f1e4e1fba9bc
Spencer Gilroy <spencer.gilroy+github@gmail.com>
2014-08-19 22:42:55 -0400

    Added Dialog Form for new Items
    Added a new dialog form for adding new items. When "New Item" or the
    plus button for an item is pressed, a dialog will appear allowing the
    user to submit a new item to the list.

--- src/js/main.js
+++ src/js/main.js
@@ -9,21 +9,49 @@
         editableOptions.theme = 'bs3';
     });
 
-    activityReporter.controller('mainCtrl', ['$scope', 'env:buildName', 'env:buildVersion', function($scope, envBuildName, envBuildVersion) {
+    activityReporter.controller('mainCtrl', ['$scope', 'env:buildName', 'env:buildVersion', '$dialogs', function($scope, envBuildName, envBuildVersion, $dialogs) {
         $scope.buildName = envBuildName;
         $scope.buildVersion = envBuildVersion;
+        var dlg = null;
 
-        $scope.items = [{text:'Top Item', subItems:[{text:'I am a top level item'}, {text:'I am also a top level item'}]},{text:'Second Item', subItems:[]}];
+        $scope.items = [{text:'Top Item', subItems:[{text:'I am a item', subItems:[]}, {text:'I am also a item', subItems:[]}]},{text:'Second Item', subItems:[]}];
 
         $scope.createNewTopItem = function(){
             console.log('Add new Topic Item!!');
-        }
+            dlg = $dialogs.create('/templates/dialog.html','dialogCtrl',{},{key: false,back: 'static'});
+            dlg.result.then(function(itemName){
+                var newItem = { text: itemName, subItems: []};
+                $scope.items.push(newItem);
+            },function(){
+                console.log('No Input');
+            });
+        };
     }]);
 
-    activityReporter.controller('dialogCtrl', ['$scope', '$rootScope', '$timeout', function($scope, $rootScope, $timeout) {
+    activityReporter.controller('dialogCtrl', ['$scope', '$modalInstance',  function($scope, $modalInstance){
+        $scope.item = {text: ''};
+
+        $scope.cancel = function(){
+            $modalInstance.dismiss('canceled');
+        }; // end cancel
+  
+        $scope.save = function(){
+            $modalInstance.close($scope.item.text);
+        }; // end save
+    }]);
+
+    activityReporter.controller('itemCtrl', ['$scope', '$rootScope', '$dialogs', function($scope, $rootScope, $dialogs) {
+        var dlg = null;
         $scope.newSubItem = function(parentItem) {
             console.log('Add new subitem');
             console.log(parentItem);
+            dlg = $dialogs.create('/templates/dialog.html','dialogCtrl',{},{key: false,back: 'static'});
+            dlg.result.then(function(itemName){
+                var newItem = { text: itemName, subItems: []};
+                parentItem.subItems.push(newItem);
+            },function(){
+                console.log('No Input');
+            });
         };
 
         $scope.editItem = function(item){
